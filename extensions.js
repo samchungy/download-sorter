ef272ab9012f082bcafa124513b8f50995156e61
Sam Chung <samchungy@gmail.com>
2018-02-12 19:17:37 +1100

    Added File Extensions & Mime Types vars

--- extensions.js
+++ extensions.js
@@ -1,3 +1,5 @@
+var defaults = {"Audio":["adp","au snd","mid midi kar rmi","m4a mp4a","mpga mp2 mp2a mp3 m2a m3a","oga ogg spx","s3m","sil","uva uvva","eol","dra","dts","dtshd","lvp","pya","ecelp4800","ecelp7470","ecelp9600","rip","weba","aac","aif aiff aifc","caf","flac","xm","mka","m3u","wax","wma","ram ra","rmp","wav"],"Image":["bmp","cgm","g3","gif","ief","jpeg jpg jpe","ktx","png","btif","sgi","svg svgz","tiff tif","psd","uvi uvvi uvg uvvg","djvu djv","sub","dwg","dxf","fbs","fpx","fst","mmr","rlc","mdi","wdp","npx","wbmp","xif","webp","3ds","ras","cmx","fh fhc fh4 fh5 fh7","ico","sid","pcx","pic pct","pnm","pbm","pgm","ppm","rgb","tga","xbm","xpm","xwd"],"Video":["3gp","3g2","h261","h263","h264","jpgv","jpm jpgm","mj2 mjp2","mp4 mp4v mpg4","mpeg mpg mpe m1v m2v","ogv","qt mov","uvh uvvh","uvm uvvm","uvp uvvp","uvs uvvs","uvv uvvv","dvb","fvt","mxu m4u","pyv","uvu uvvu","viv","webm","f4v","fli","flv","m4v","mkv mk3d mks","mng","asf asx","avi","vob","wm","wmv","wmx","wvx","movie","smv"],"Font":["ttc","otf","ttf","woff","woff2"],"Compressed":["7z","cbr cba cbt cbz cb7","deb udeb","rar","sitx","zip"],"Office":["csv","doc dot","docm","docx","dotm","dotx","odb","odc","odf","odft","odg","odi","odm","odp","ods","odt","onetoc onetoc2 onetmp onepkg","otc","otg","oth","oti","otp","ots","ott","pdf","potm","potx","ppam","ppd","ppsm","ppsx","ppt pps pot","pptm","pptx","pub","sldm","sldx","txt text conf def list log in","xls xlm xla xlc xlt xlw","xlsb","xlsm","xlsx","xltm","xltx","xml xsl"],"Web":["cer","css","html htm","js","rss","xhtml xht"],"Developer":["c cc cxx cpp h hh dic","class","f for f77 f90","java","json","jsonml","p pas"],"Executable":["apk","exe dll com bat msi","jar"],"Other":["123","3dml","aab x32 u32 vox","aam","aas","abw","ac","acc","ace","acu","aep","afp listafp list3820","ahead","ai eps ps","air","ait","ami","appcache","application","apr","arc","asc sig","aso","atc acutc","atom","atomcat","atomsvc","atx","aw","azf","azs","azw","bcpio","bdf","bdm","bed","bh2","bin dms lrf mar so dist distz pkg bpk dump elc deploy","blb blorb","bmi","box","bz","bz2 boz","c11amc","c11amz","c4g c4d c4f c4p c4u","cab","car","cat","ccxml","cdbcmsg","cdkey","cdmia","cdmic","cdmid","cdmio","cdmiq","cdx","cdxml","cdy","cfs","chat","chm","chrt","cif","cii","cil","cla","clkk","clkp","clkt","clkw","clkx","clp","cmc","cmdf","cml","cmp","cod","cpio","cpt","crd","crl","cryptonote","csh","csml","csp","cu","curl","cww","dae","daf","dart","davmount","dbk","dcurl","dd2","ddd","der crt","dfac","dgc","dir dcr dxr cst cct cxt w3d fgd swa","dis","dmg","dna","dp","dpg","dsc","dssc","dtb","dtd","dvi","dwf","dxp","ecma","edm","edx","efif","ei6","eml mime","emma","eot","epub","es3 et3","esa","esf","etx","eva","evy","exi","ext","ez","ez2","ez3","fcdt","fcs","fdf","fe_launch","fg5","fig","flo","flw","flx","fly","fm frame maker book","fnc","fsc","ftc","fti","fxp fxpl","fzs","g2w","g3w","gac","gam","gbr","gca","gdl","geo","gex gre","ggb","ggt","ghf","gim","gml","gmx","gnumeric","gph","gpx","gqf gqs","gram","gramps","grv","grxml","gsf","gtar","gtm","gtw","gv","gxf","gxt","hal","hbci","hdf","hlp","hpgl","hpid","hps","hqx","htke","hvd","hvp","hvs","i2g","icc icm","ice","ics ifb","ifm","igl","igm","igs iges","igx","iif","imp","ims","ink inkml","install","iota","ipfix","ipk","irm","irp","iso","itp","ivp","ivu","jad","jam","jisp","jlt","jnlp","joda","karbon","kfo","kia","kml","kmz","kne knp","kon","kpr kpt","kpxx","ksp","ktz ktr","kwd kwt","lasxml","latex","lbd","lbe","les","link66","lnk","lostxml","lrm","ltf","lwp","lzh lha","m21 mp21","m3u8","ma nb mb","mads","mag","mathml","mbk","mbox","mc1","mcd","mcurl","mdb","meta4","metalink","mets","mfm","mft","mgp","mgz","mie","mif","mlp","mmd","mmf","mny","mods","mp4s","mpc","mpkg","mpm","mpn","mpp mpt","mpy","mqy","mrc","mrcx","mscml","mseed","mseq","msf","msh mesh silo","msl","msty","mts","mus","musicxml","mvb m13 m14","mwf","mxf","mxl","mxml xhvml xvml xvm","mxs","n3","nbp","nc cdf","ncx","nfo","n-gage","ngdat","nlu","nml","nnd","nns","nnw","nsc","nsf","ntf nitf","nzb","oa2","oa3","oas","obd","obj","oda","ogx","omdoc","opf","opml","org","osf","osfpvg","oxps","oxt","p10","p12 pfx","p7b spc","p7m p7c","p7r","p7s","p8","paw","pbd","pcap cap dmp","pcf","pcl","pclxl","pcurl","pdb pqa oprc","pfa pfb pfm afm","pfr","pgn","pgp","pki","pkipath","plb","plc","plf","pls","pml","portpkg","prc mobi","pre","prf","psb","psf","pskcxml","ptid","pvb","pwn","qam","qbo","qfx","qps","qxd qxt qwd qwt qxl qxb","rcprofile","rdf","rdz","rep","res","rif","ris","rl","rld","rm","rms","rmvb","rnc","roa","rp9","rpss","rpst","rq","rs","rsd","rtf","rtx","s asm","saf","sbml","sc","scd","scm","scq","scs","scurl","sda","sdc","sdd","sdkm sdkd","sdp","sdw vor","see","seed dataless","sema","semd","semf","ser","setpay","setreg","sfd-hdstx","sfs","sfv","sgl","sgml sgm","sh","shar","shf","sis sisx","sit","skp skd skt skm","slt","sm","smf","smi smil","smzip","snf","spf","spl","spot","spp","spq","sql","src","srt","sru","srx","ssdl","sse","ssf","ssml","st","stc","std","stf","sti","stk","stl","str","stw","sub","sus susp","sv4cpio","sv4crc","svc","svd","swf","swi","sxc","sxd","sxg","sxi","sxm","sxw","t tr roff man me ms","t3","taglet","tao","tar","tcap","tcl","teacher","tei teicorpus","tex","texinfo texi","tfi","tfm","thmx","tmo","torrent","tpl","tpt","tra","trm","tsd","tsv","ttl","twd twds","txd","txf","ufd ufdl","ulx","umj","unityweb","uoml","uri uris urls","ustar","utz","uu","uvf uvvf uvd uvvd","uvt uvvt","uvx uvvx","uvz uvvz","vcard","vcd","vcf","vcg","vcs","vcx","vis","vsd vst vss vsw","vsf","vtu","vxml","wad","wbs","wbxml","wg","wgt","wmd","wmf wmz emf emz","wml","wmlc","wmls","wmlsc","wmz","wpd","wpl","wps wks wcm wdb","wqd","wri","wrl vrml","wsdl","wspolicy","wtb","x3d x3dz","x3db x3dbz","x3dv x3dvz","xaml","xap","xar","xbap","xbd","xdf","xdm","xdp","xdssc","xdw","xenc","xer","xfdf","xfdl","xlam","xlf","xo","xop","xpi","xpl","xpr","xps","xpw xpx","xslt","xsm","xspf","xul","xyz","xz","yang","yin","z1 z2 z3 z4 z5 z6 z7 z8","zaz","zir zirz","zmm"]}
+
 jQuery(document).ready(function() {
     jQuery('.tabs .tab-links a').on('click', function(e)  {
         var currentAttrValue = jQuery(this).attr('href');
@@ -9,5 +11,31 @@ jQuery(document).ready(function() {
         jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
 
         e.preventDefault();
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
     });
 });
+
+function onLoad(){
+
+
+    $.each( defaults, function( key, value ) {
+        $('#file_ext tr:last').after('</tr><tr><td>'+key+'</td><td>'+value+'</td></tr>');
+        console.log(key);
+    });
+}
+
+function save_file_extensions(){
+    chrome.storage.sync.set(defaults
+    , function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+document.getElementById('save_file_ext').addEventListener('click',
+    save_options);
+document.addEventListener('DOMContentLoaded', onLoad);
